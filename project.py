9a68e1f76f5e6d8255b9f9391b6d5e26cd8f627e
lodwr <l.odwrot@gmail.com>
2019-05-28 17:36:03 +0200

    Added NM and confusion matrix calculation

--- project.py
+++ project.py
@@ -7,12 +7,13 @@ import numpy as np
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2, mutual_info_classif
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-from stats import calculateStatsForKMeans
+from stats import calculateStatsForKMeans, calculateStatsForNM
 from dataLoad import getDataSetLeukemia, getDataSetIris
 
-# Load Data
+# Load Data (param whether data should be normalized)
 data = getDataSetLeukemia(False)
 # data = getDataSetIris()
+# K - class
 X = data.drop('K', axis = 1)
 y = data.K
 
@@ -36,5 +37,15 @@ for i in range (1, len(sortedFeatures) + 1):
     for dropedFeatureIndex in range (i, len(sortedFeatures)):
         subData = subData.drop(sortedFeatures[dropedFeatureIndex], axis=1)
     print('Stats for features: ', i)
-    print(calculateStatsForKMeans(subData, 10))
+    # Print stats For NM
+    accuracy, precision, recall, fscore, confusionMatrix = calculateStatsForNM(subData, 'euclidean', True)
+    # Print stats for 1-NN
+    # accuracy, precision, recall, fscore, confusionMatrix = calculateStatsForKMeans(subData, 1, 'euclidean')
+    # # Print stats for 10-NN
+    # accuracy, precision, recall, fscore, confusionMatrix = calculateStatsForKMeans(subData, 10, 'euclidean')
+    # # Print stats for 20-NN
+    # accuracy, precision, recall, fscore, confusionMatrix = calculateStatsForKMeans(subData, 20, 'euclidean')
+    print('Stats', accuracy, precision, recall, fscore)
+    print('Confusion Matrix')
+    print(confusionMatrix)
 
--- stats.py
+++ stats.py
@@ -2,6 +2,9 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.model_selection import KFold
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors.nearest_centroid import NearestCentroid
+from sklearn.metrics import confusion_matrix
+import numpy as np
 
 def countStats(_y_true, _y_pred):
   accuracy = accuracy_score(_y_true, _y_pred, normalize=True)
@@ -11,11 +14,15 @@ def countStats(_y_true, _y_pred):
   
   return accuracy, precision, recall, fscore
 
-def calculateStatsForKMeans(data, k=2, metric='minkowski'):
+def calculateStatsForKMeans(data, k=2, metric='euclidean'):
   tAccuracy = 0
   tPrecision = 0
   tRecall = 0
   tFScore = 0
+
+  # Data used to draw confusion matrix will be gathered here
+  ConfusionMatrixY = []
+  ConfusionMatrixYpred = []
   
   for i in range(0, 5):
     kf = KFold(n_splits = 2, shuffle = True)
@@ -35,12 +42,17 @@ def calculateStatsForKMeans(data, k=2, metric='minkowski'):
     Xtest = test1.drop('K', axis = 1)
     Ytest = test1.K
     YPred = neigh.predict(Xtest)
+
+    if i == 0:
+        ConfusionMatrixY = Ytest.tolist()
+        ConfusionMatrixYpred = YPred.tolist()
     
     accuracy, precision, recall, fscore = countStats(Ytest, YPred)
     tAccuracy += accuracy
     tPrecision += precision
     tRecall += recall
     tFScore += fscore
+
     
     # Check second fold
     neigh = KNeighborsClassifier(n_neighbors=k, metric=metric)
@@ -51,11 +63,84 @@ def calculateStatsForKMeans(data, k=2, metric='minkowski'):
     Xtest = test2.drop('K', axis = 1)
     Ytest = test2.K
     YPred = neigh.predict(Xtest)
+
+    if i == 0:
+        ConfusionMatrixY = ConfusionMatrixY + Ytest.tolist()
+        ConfusionMatrixYpred = ConfusionMatrixYpred + YPred.tolist()
     
     accuracy, precision, recall, fscore = countStats(Ytest, YPred)
     tAccuracy += accuracy
     tPrecision += precision
     tRecall += recall
     tFScore += fscore
+
+    # Create and normalize confusion matrix
+    cm = confusion_matrix(ConfusionMatrixY, ConfusionMatrixYpred)
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
     
-  return tAccuracy/10, tPrecision/10, tRecall/10, tFScore/10
+  return tAccuracy/10, tPrecision/10, tRecall/10, tFScore/10, cm
+
+def calculateStatsForNM(data, metric='euclidean', drawConfusionMatrix=False):
+    tAccuracy = 0
+    tPrecision = 0
+    tRecall = 0
+    tFScore = 0
+
+    # Data used to draw confusion matrix will be gathered here
+    ConfusionMatrixY = []
+    ConfusionMatrixYpred = []
+  
+    for i in range(0, 5):
+        kf = KFold(n_splits = 2, shuffle = True)
+        result = next(kf.split(data), None)
+
+        train1 = data.iloc[result[0]]
+        test1 =  data.iloc[result[1]]
+        train2 = data.iloc[result[1]]
+        test2 =  data.iloc[result[0]]
+
+        # Check first fold
+        nm = NearestCentroid(metric=metric)
+        Xtrain = train1.drop('K', axis = 1)
+        Ytrain = train1.K
+        nm.fit(Xtrain, Ytrain)
+
+        Xtest = test1.drop('K', axis = 1)
+        Ytest = test1.K
+        YPred = nm.predict(Xtest)
+
+        if i == 0:
+            ConfusionMatrixY = Ytest.tolist()
+            ConfusionMatrixYpred = YPred.tolist()
+
+        accuracy, precision, recall, fscore = countStats(Ytest, YPred)
+        tAccuracy += accuracy
+        tPrecision += precision
+        tRecall += recall
+        tFScore += fscore
+
+        # Check second fold
+        nm = NearestCentroid(metric=metric)
+        Xtrain = train2.drop('K', axis = 1)
+        Ytrain = train2.K
+        nm.fit(Xtrain, Ytrain)
+
+        Xtest = test2.drop('K', axis = 1)
+        Ytest = test2.K
+        YPred = nm.predict(Xtest)
+
+        if i == 0:
+            ConfusionMatrixY = ConfusionMatrixY + Ytest.tolist()
+            ConfusionMatrixYpred = ConfusionMatrixYpred + YPred.tolist()
+
+        accuracy, precision, recall, fscore = countStats(Ytest, YPred)
+        tAccuracy += accuracy
+        tPrecision += precision
+        tRecall += recall
+        tFScore += fscore
+
+        # Create and normalize confusion matrix
+        cm = confusion_matrix(ConfusionMatrixY, ConfusionMatrixYpred)
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+    return tAccuracy/10, tPrecision/10, tRecall/10, tFScore/10, cm
