65cb556b187590d791bd33f2fe005d27b3f86401
Torak28 <zelechowski28@gmail.com>
2019-06-09 22:56:27 +0200

    Prepare to write Report :fire:

--- project.py
+++ project.py
@@ -3,12 +3,15 @@ from mpl_toolkits.mplot3d import Axes3D
 from sklearn import datasets
 from sklearn.decomposition import PCA
 import pandas as pd
+from pandas.plotting import scatter_matrix
 import numpy as np
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2, mutual_info_classif
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn import preprocessing
 from stats import calculateStatsForKMeans, calculateStatsForNM
 from dataLoad import getDataSetLeukemia, getDataSetIris
+import seaborn as sns
 
 # Load Data (param whether data should be normalized)
 data = getDataSetLeukemia(False)
@@ -16,6 +19,14 @@ data = getDataSetLeukemia(False)
 # K - class
 X = data.drop('K', axis = 1)
 y = data.K
+
+'''
+# DANE O INDEXIE
+print(f'dtypes: {data.dtypes}')
+print(f'y: {y}')
+print(f'y[-1]: {y.iloc[-1]}')
+print(f'type(y[-1]): {type(y.iloc[-1])}')
+'''
 
 # Build features ranking
 test = SelectKBest(score_func=chi2, k=2)
@@ -29,23 +40,62 @@ for i in range(num_features):
 
 sortedFeatures = sorted(scores, reverse = True)
 sortedFeatures = list(map(lambda x: x[1], sortedFeatures))
-print ('Features Ranking', sortedFeatures)
 
-# Count stats for params
-for i in range (1, len(sortedFeatures) + 1):
-    subData = data.copy()
-    for dropedFeatureIndex in range (i, len(sortedFeatures)):
-        subData = subData.drop(sortedFeatures[dropedFeatureIndex], axis=1)
-    print('Stats for features: ', i)
-    # Print stats For NM
-    accuracy, precision, recall, fscore, confusionMatrix = calculateStatsForNM(subData, 'euclidean', True)
-    # Print stats for 1-NN
-    # accuracy, precision, recall, fscore, confusionMatrix = calculateStatsForKMeans(subData, 1, 'euclidean')
-    # # Print stats for 10-NN
-    # accuracy, precision, recall, fscore, confusionMatrix = calculateStatsForKMeans(subData, 10, 'euclidean')
-    # # Print stats for 20-NN
-    # accuracy, precision, recall, fscore, confusionMatrix = calculateStatsForKMeans(subData, 20, 'euclidean')
-    print('Stats', accuracy, precision, recall, fscore)
-    print('Confusion Matrix')
-    print(confusionMatrix)
+# Wyniki - dane
+'''
+# INFO
+print(data)
+print(f'Zależność liniowa(powinna być równa ilości kolumn): {np.linalg.matrix_rank(data)}')
+print(f'Kształt danych: {data.shape}')
+print(f'Cechy kluczowe: {sortedFeatures}')
+print(f'Długość cech kluczowych: {len(sortedFeatures)}')
+print(f'Wymiar danyych: {data.ndim}')
+print(f'Info: {data.info(verbose=True)}')
+print(f'data.T: {data.T}')
+'''
 
+
+''' 
+# PRINT DATAFRAME
+print(data.to_string(index=False, header=False, columns=data.columns.tolist()[:-1]), file=open('dataTest.txt', 'w'))
+print(data.to_string(), file=open('dataTest.txt', 'w'))
+print(f'data.T.dot(data): {data.T.dot(data).to_string()}', file=open('dataTDot.txt', 'w'))
+'''
+
+
+# WYKRES
+sns.set()
+sns.set_palette(sns.color_palette("hls", len(sortedFeatures)))
+# sns.pairplot(data, vars=sortedFeatures, hue='K', diag_kind='kde', dropna=True, kind='scatter')
+sns.pairplot(data, vars=sortedFeatures, hue='K', diag_kind='hist', dropna=True, kind='scatter')
+# plt.show()
+plt.savefig('foo.png')
+
+
+
+# Wyniki
+# print (f'Features Ranking {sortedFeatures}', file=open('result.txt', 'w'))
+
+
+# # Count stats for params
+# for i in range (1, len(sortedFeatures) + 1):
+#     subData = data.copy()
+#     for dropedFeatureIndex in range (i, len(sortedFeatures)):
+#         subData = subData.drop(sortedFeatures[dropedFeatureIndex], axis=1)
+#     print(f'Stats for features: {i}', file=open('result.txt', 'a'))
+#     # Print stats For NM
+#     accuracy, precision, recall, fscore, confusionMatrix = calculateStatsForNM(subData, 'euclidean', True)
+#     print(f'Stats NM: \n\t Accuracy: {accuracy} \n\t Precision: {precision} \n\t Recall: {recall} \n\t Fscore: {fscore} \n\t Confusion Matrix: \n {confusionMatrix}', file=open('result.txt', 'a'))
+
+#     # Print stats for 1-NN
+#     accuracy, precision, recall, fscore, confusionMatrix = calculateStatsForKMeans(subData, 1, 'euclidean')
+#     print(f'Stats 1-NN: \n\t Accuracy: {accuracy} \n\t Precision: {precision} \n\t Recall: {recall} \n\t Fscore: {fscore} \n\t Confusion Matrix: \n {confusionMatrix}', file=open('result.txt', 'a'))
+
+#     # # Print stats for 10-NN
+#     accuracy, precision, recall, fscore, confusionMatrix = calculateStatsForKMeans(subData, 10, 'euclidean')
+#     print(f'Stats 10-NN: \n\t Accuracy: {accuracy} \n\t Precision: {precision} \n\t Recall: {recall} \n\t Fscore: {fscore} \n\t Confusion Matrix: \n {confusionMatrix}', file=open('result.txt', 'a'))
+
+#     # # Print stats for 20-NN
+#     accuracy, precision, recall, fscore, confusionMatrix = calculateStatsForKMeans(subData, 20, 'euclidean')
+#     print(f'Stats 20-NN: \n\t Accuracy: {accuracy} \n\t Precision: {precision} \n\t Recall: {recall} \n\t Fscore: {fscore} \n\t Confusion Matrix: \n {confusionMatrix}', file=open('result.txt', 'a'))
+#     print(f'------------------', file=open('result.txt', 'a'))
