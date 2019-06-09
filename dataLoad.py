7f4b2c14d5d1d75dd2e6d315183b568c9383bf60
Torak28 <zelechowski28@gmail.com>
2019-06-09 20:24:35 +0200

    Change data that is load to make it float64

--- dataLoad.py
+++ dataLoad.py
@@ -4,7 +4,10 @@ import numpy as np
 
 def getDataSetLeukemia(normalized=False):
     url = 'https://raw.githubusercontent.com/lOdwrot/MedycynaBialaczka/master/data/data.csv'
-    data=pd.read_csv(url, sep =";")
+    data = pd.read_csv(url, sep =";", dtype='float64')
+    cols = data.columns.tolist()
+    cols = cols[1:] + cols[:1]
+    data = data[cols]
     features = list(filter(lambda x: x != 'K', data.columns))
     if normalized == True:
         data[features] = data[features].apply(lambda x: (x - x.min()) / (x.max() - x.min()))
